097daca2a2336ae7b50ae416a1ec5000ecb64c93
Alexandre Peinado <apeinadollopis@gmail.com>
2023-12-19 10:44:28 +0100

    Initial Commit

---
+++ ProyectoMisMangas/ProyectoMisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoMisMangasApp.swift
+//  ProyectoMisMangas
+//
+//  Created by Alex Peinado Llopis on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoMisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
